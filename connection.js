2b85a31514465339006e07b464a13e99ed551753
AdityaGhamat <127591208+AdityaGhamat@users.noreply.github.com>
2023-12-28 05:48:28 -0800

    added backend

---
+++ connection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const connectMongoDb = async () => {
+  return mongoose
+    .connect(
+      "mongodb+srv://adityaghamat01:Aditya01@camera.r3k620o.mongodb.net/"
+    )
+    .then(() => console.log("MongoDb connection established"))
+    .catch((err) => console.log("error while establishing connection", err));
+};
+module.exports = {
+  connectMongoDb,
+};
